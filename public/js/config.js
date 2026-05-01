36ed9b4179537d8a1b20fa158e3d8183c0d6b0f6
untown <yusufyilmxz@gmail.com>
2026-05-01 16:26:30 +0300

    Delete public directory

--- public/js/config.js
+++
@@ -1,8 +0,0 @@
-// Şifreleri değiştirmek istersen sadece burayı düzenle.
-// Kullanıcının istediğine göre Türkçe büyük İ içerir.
-window.APP_CONFIG = {
-  SITE_PASSWORD: 'Admin',
-  ADMIN_PASSWORD: 'Admin',
-  SESSION_KEY: 'yk_session_v1',
-  ADMIN_SESSION_KEY: 'yk_admin_session_v1'
-};
