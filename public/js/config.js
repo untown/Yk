d55a981cb7f449f8ffa3f786cd2d10d7e7fe0567
untown <yusufyilmxz@gmail.com>
2026-05-01 15:16:31 +0300

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 // Şifreleri değiştirmek istersen sadece burayı düzenle.
 // Kullanıcının istediğine göre Türkçe büyük İ içerir.
 window.APP_CONFIG = {
-  SITE_PASSWORD: 'Yİlmaz',
-  ADMIN_PASSWORD: 'Yİlmaz',
+  SITE_PASSWORD: 'Admin',
+  ADMIN_PASSWORD: 'Admin',
   SESSION_KEY: 'yk_session_v1',
   ADMIN_SESSION_KEY: 'yk_admin_session_v1'
 };
